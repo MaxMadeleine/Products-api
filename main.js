28e4c6bdb80364de1e84814aad273a8a00869bc3
MaxMadeleine <Max.Madeleine.G@gmail.com>
2024-09-24 08:44:09 +0200

    nedds to build veiw everthing ele is good

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-
 let app = document.getElementById("app");
 //view
+//når search bliver trykket sender den input value til searchItemCallback
 function buildSearch() {
   app.innerHTML = `
     <div class="container">
@@ -9,33 +9,51 @@ function buildSearch() {
     </div>`;
 }
 //controller
+//inputValue bliver det samme som value fra searchInput og sendt til getValueToUrl
 function searchItemCallback() {
-    const inputValue = document.getElementById("searchInput").value;
-    console.log(inputValue);
-    getValueToUrl(inputValue)
-
+  const inputValue = document.getElementById("searchInput").value;
+  console.log(inputValue);
+  getValueToUrl(inputValue);
 }
 //model
+// når getValueToUrl får inputValue køre den fetc ( inputValue bliver sat i parameter så vi kan kalde på den nede i url )
 function getValueToUrl(inputValue) {
-    fetch(`https://dummyjson.com/products/search?q=${inputValue}`) 
+  fetch(`https://dummyjson.com/products/search?q=${inputValue}`)
+    // så tager den data og putter i response
     .then((response) => {
-
-        console.log(response);
-        
-       return response.json()
+      if (!response.ok) {
+        return console.log(new Error("error"));
+      }
+      return response.json();
     })
-    if (response.ok) {
-        return console.log(Error);
-    }
-    
+    //laver response om til json og tager json og putter i data
+    .then((data) => {
+      console.log(data);
+      displayProducts(data.products);
+    });
 }
 
-buildSearch()
+function productView(products) {
 
+let productHTML
 
+}
 
+//product view
+function productView(products) {
+  app.innerHTML = `
+     <div class="productContainer">${product}</div>
+    `;
+  products.forEach((product) => {
+    `   <figure>
+    <img src="${product.image}" alt="${product.name}">
+    <figcaption>
+    <h3>${product.name}</h3>
+    <p>Price: $${product.price}</p>
+    <p>${product.description}</p>
+</figcaption>
+</figure>`;
+  });
+}
 
-    
-    
-    
-    
+buildSearch();
